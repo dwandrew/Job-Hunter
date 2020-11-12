152f00c2ff74388feb8c1d8fa31e7996f98955ed
dwandrew <danwandrew@gmail.com>
2020-11-12 14:49:02 +0000

    added new static controller and homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :companies
-  root 'jobs#index'
+  root 'static#home'
   resources :jobs
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
